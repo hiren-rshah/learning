d3dacc2e1cb720a834159cdea9d9fd8f2c8f828b
hireniya <hireniya@gmail.com>
2019-03-13 10:10:48 +0530

    Get/Post Controller Test in DateTimeCntr

--- learning/src/main/java/com/self/learning/cntr/DateTimeCntr.java
+++ learning/src/main/java/com/self/learning/cntr/DateTimeCntr.java
@@ -1,33 +1,84 @@
 package com.self.learning.cntr;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.net.URL;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Objects;
-import java.util.Optional;
+import com.self.learning.service.DateTimeService;
 
 @RestController
 public class DateTimeCntr {
 
-    @RequestMapping(value = {"/currentTime", "/currentTime/{timeZone}"}, method = {RequestMethod.GET})
-    public String getTime(@PathVariable(value = "timeZone", required = false) Optional<String> timeZoneStr) {
-        LocalDateTime now = LocalDateTime.now();
-        System.out.println("timeZoneStr : " + timeZoneStr);
-        System.out.println("Optional.of(timeZoneStr).orElse(null)) : " + Optional.of(timeZoneStr).orElse(null));
-        if ("PST".equals(timeZoneStr.isPresent() ? timeZoneStr.get() : null)) {
-            return convertTime("Europe/Paris");
-        } else if ("EST".equals(timeZoneStr.isPresent() ? timeZoneStr.get() : null)) {
-            return convertTime("America/New_York");
-        } else {
-            return convertTime("Asia/Kolkata");
-        }
-    }
+	private DateTimeService dateTimeService;
 
-    private String convertTime(String zone) {
-        return ZonedDateTime.now(ZoneId.of(zone)).format(DateTimeFormatter.ofPattern("dd MMM yyyy hh"));
-    }
+	public DateTimeCntr(DateTimeService dateTimeService) {
+		this.dateTimeService = dateTimeService;
+	}
+	
+	@RequestMapping(value = { "/currentTime", "/currentTime/{timeZone}" }, method = { RequestMethod.GET })
+	public String getTime(@PathVariable(value = "timeZone", required = false) String timeZoneStr) {
+		if ("PST".equals(timeZoneStr)) {
+			return dateTimeService.convertTime("Europe/Paris");
+		} else if ("EST".equals(timeZoneStr)) {
+			return dateTimeService.convertTime("America/New_York");
+		} else {
+			return dateTimeService.convertTime("Asia/Kolkata");
+		}
+	}
+
+	@GetMapping(value = { "/currentTimeJsonGet",  "/currentTimeJsonGet/{timeZone}"}, produces = MediaType.APPLICATION_JSON_VALUE)
+	public MyTime getJsonTime(@PathVariable(value = "timeZone", required = false) String timeZoneStr) {
+		System.out.println("getJsonTime - Received timeZone is : " + timeZoneStr);
+		if ("PST".equals(timeZoneStr)) {
+			return new MyTime(timeZoneStr,dateTimeService.convertTime("Europe/Paris"));
+		} else if ("EST".equals(timeZoneStr)) {
+			return new MyTime(timeZoneStr,dateTimeService.convertTime("America/New_York"));
+		} else {
+			return new MyTime(timeZoneStr,dateTimeService.convertTime("Asia/Kolkata"));
+		}
+	}
+	
+	@PostMapping(value = { "/currentTimeJsonPost",  "/currentTimeJsonPost/{timeZone}"}, consumes = MediaType.APPLICATION_JSON_VALUE ,produces = MediaType.APPLICATION_JSON_VALUE)
+	public MyTime postJsonTime(@PathVariable(value = "timeZone", required = false) String timeZoneStr) {
+		System.out.println("postJsonTime - Received timeZone is : " + timeZoneStr);
+		if ("PST".equals(timeZoneStr)) {
+			return new MyTime(timeZoneStr,dateTimeService.convertTime("Europe/Paris"));
+		} else if ("EST".equals(timeZoneStr)) {
+			return new MyTime(timeZoneStr,dateTimeService.convertTime("America/New_York"));
+		} else {
+			return new MyTime(timeZoneStr,dateTimeService.convertTime("Asia/Kolkata"));
+		}
+	}
+
+	private class MyTime {
+		private String timeZone;
+		private String dateTime;
+
+		public String getTimeZone() {
+			return timeZone;
+		}
+
+		public void setTimeZone(String timeZone) {
+			this.timeZone = timeZone;
+		}
+
+		public String getDateTime() {
+			return dateTime;
+		}
+
+		public void setDateTime(String dateTime) {
+			this.dateTime = dateTime;
+		}
+
+		public MyTime(String timeZoneStr, String dateTime) {
+			this.timeZone = timeZoneStr;
+			this.dateTime = dateTime;
+		}
+
+	}
 }
--- learning/src/test/java/com/self/learning/cntr/DateTimeCntrTest.java
+++ learning/src/test/java/com/self/learning/cntr/DateTimeCntrTest.java
@@ -1,31 +1,90 @@
 package com.self.learning.cntr;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import java.text.DateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Optional;
 
-import static org.junit.Assert.*;
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-@RunWith(SpringRunner.class)
-@WebMvcTest(value = DateTimeCntr.class, secure = false)
+import com.self.learning.service.DateTimeService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
 public class DateTimeCntrTest {
 
-    @Autowired
-    DateTimeCntr dayTimeCntr;
+	private MockMvc mockMvc;
 
-    @Test
-    public void getTimeTest(){
-        //assertEquals("11 Mar 2019 04", dayTimeCntr.getTime(Optional.of("IST")));
-        LocalDateTime now = LocalDateTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMM yyyy hh");
-        assertEquals(now.format(formatter), dayTimeCntr.getTime(Optional.of("IST")));
-    }
+	@Mock
+	private DateTimeService dateTimeService;
+
+	@InjectMocks
+	private DateTimeCntr dateTimeCntr;
+
+	@Before
+	public void setUp() throws Exception {
+		mockMvc = MockMvcBuilders.standaloneSetup(dateTimeCntr).build();
+	}
+	
+	@Test
+	public void testIndianTime() throws Exception {
+
+		LocalDateTime now = LocalDateTime.now();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMM yyyy hh:mm");
+
+		Mockito.when(dateTimeService.convertTime("Asia/Kolkata")).thenReturn(now.format(formatter));
+		
+		mockMvc.perform(MockMvcRequestBuilders.get("/currentTime/IST"))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.content().string(now.format(formatter)));
+		
+		Mockito.verify(dateTimeService).convertTime("Asia/Kolkata");
+	}
+	
+	@Test
+	public void testGetJsonTime() throws Exception {
+		LocalDateTime now = LocalDateTime.now();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMM yyyy hh:mm");
+		
+		Mockito.when(dateTimeService.convertTime("Asia/Kolkata")).thenReturn(now.format(formatter));
+		
+		mockMvc.perform(MockMvcRequestBuilders.get("/currentTimeJsonGet/IST")
+				.accept(MediaType.APPLICATION_JSON_VALUE))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.timeZone", Matchers.is("IST")))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.dateTime", Matchers.is(now.format(formatter))))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.*", Matchers.hasSize(2)));
+		Mockito.verify(dateTimeService).convertTime("Asia/Kolkata");
+	}
+	
+	@Test
+	public void testPostJsonTime() throws Exception {
+		
+		LocalDateTime now = LocalDateTime.now();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMM yyyy hh:mm");
+		
+		
+		Mockito.when(dateTimeService.convertTime("Europe/Paris")).thenReturn(now.format(formatter));
+		
+		String postData = "{\"timeZone\": PST\"}";
+		mockMvc.perform(MockMvcRequestBuilders.post("/currentTimeJsonPost/PST")
+				.contentType(MediaType.APPLICATION_JSON_VALUE)
+				.content(postData)
+				.accept(MediaType.APPLICATION_JSON_VALUE))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.timeZone", Matchers.is("PST")))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.dateTime", Matchers.is(now.format(formatter))))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.*",Matchers.hasSize(2)));
+		Mockito.verify(dateTimeService).convertTime("Europe/Paris");
+		
+	}
 }
